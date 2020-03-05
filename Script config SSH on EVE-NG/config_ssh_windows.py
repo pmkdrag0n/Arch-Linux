4aed774ee3ca7711961055f9569419ae41e2cb7d
BlackDrag0n <pmk.drag0n.1998@gmail.com>
2020-03-05 22:06:30 +0700

    version 2.0

--- Script config SSH on EVE-NG/config_ssh_windows.py
+++ Script config SSH on EVE-NG/config_ssh_windows.py
@@ -1,29 +1,79 @@
 from netmiko import ConnectHandler
-ip_sv=input("Nhap IP server EVE-NG: ")
-n=int(input("So luong thiet bi: "))
-porttn=32769
-config_int_e0=["int e0/0",
-"no sw",
-"no shut",
-"ip add dhcp"
-]
-config_ssh=[
-"enable pass 321",
-"username admin pass 123",
-"ip domain-name pmk",
-"crypto key ge rsa modul 1024",
-"line vty 0 4",
-"login local",
-"pass 123"
-]
-for x in range (n):
-    telnet= ConnectHandler(device_type="cisco_ios_telnet",ip=ip_sv,port=porttn,secret="321",blocking_timeout=16)
-    telnet.enable()
-    print("Configuring device "+ str(x+1)+"...")
-    telnet.send_config_set(config_int_e0)
-    telnet.send_config_set(config_ssh)
-    output = telnet.send_command("show ip int brief",delay_factor=12)
-    print(output)
-    print("#"*50)
-    porttn=porttn+1
-    telnet.disconnect()
+from multiprocessing import Process
+import time
+def config_ssh(a_device):
+        config_int_e0=["int e0/0",
+        "no sw",
+        "no shut",
+        "ip add dhcp"
+        ]
+        config_ssh=[
+        "enable pass 321",
+        "username admin pass 123",
+        "ip domain-name pmk",
+        "crypto key ge rsa modul 1024",
+        "line vty 0 15",
+        "login local",
+        "pass 123"
+        ]
+        telnet = ConnectHandler(**a_device)
+        telnet.enable()
+        telnet.send_config_set(config_int_e0)
+        telnet.send_config_set(config_ssh)
+        telnet.disconnect()
+def show_ip(a_device):
+        telnet=ConnectHandler(**a_device)
+        print("*"*80)
+        print(telnet.send_command_expect("show ip int br",delay_factor=30))
+        print("*"*80)
+def list_menu():
+    print("1.Cau hinh SSH cho cac thiet bi")
+    print("2.Show ip interface brief")
+    print("0.Thoat")
+def menu():
+    list_menu()
+    choice=int(input("Nhap lua chon cua ban: "))
+    if choice==1:
+        print("Bat dau gui cau hinh len thiet bi...")
+        command=config_ssh
+        procs = []
+        for a_device in devices:
+            my_proc = Process(target=command, args=(a_device,))
+            my_proc.start()
+            procs.append(my_proc)
+        time.sleep(10)
+        print("Hoan tat cong viec")
+        menu()
+    elif choice==2:
+        print("Bat dau thi nhap IP va xuat ra file")
+        command=show_ip
+        procs = []
+        for a_device in devices:
+            my_proc = Process(target=command, args=(a_device,))
+            my_proc.start()
+            procs.append(my_proc)
+        time.sleep(10)
+        print("Hoan tat cong viec")
+        menu()
+    elif choice==0:
+        sys.exit
+    else:
+        print("Invalid choice selected")
+
+if __name__ == '__main__':
+    ip_sv=input("Nhap IP server EVE-NG: ")
+    sl=int(input("Nhap so luong thiet bi co trong so do mang: "))
+    sw={
+            "device_type" : "cisco_ios_telnet",
+            "ip" : ip_sv,
+            "port" :"32769",
+            "secret":"321"
+            }
+    portt=32769
+    devices=[]
+    for n in range (sl):
+        sw1=sw.copy()
+        sw1["port"]=str(portt)
+        devices.append(sw1)
+        portt=portt+1
+    menu()
