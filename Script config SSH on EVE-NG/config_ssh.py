b17c32fd83674711fed0c25549cad286738292d9
BlackDrag0n <pmk.drag0n.1998@gmail.com>
2020-03-06 03:58:04 +0700

    add some command

--- Script config SSH on EVE-NG/config_ssh.py
+++ Script config SSH on EVE-NG/config_ssh.py
@@ -1,4 +1,4 @@
-from netmiko import Netmiko
+from netmiko import ConnectHandler
 from ntc_templates.parse import parse_output
 from datetime import datetime
 def config(sl,ip_sv,porttel=32769):
@@ -12,46 +12,33 @@ def config(sl,ip_sv,porttel=32769):
     "username admin pass 123",
     "ip domain-name pmk",
     "crypto key ge rsa modul 1024",
-    "line vty 0 15",
+    "line vty 0 4",
     "login local",
     "pass 123"
     ]
     for x in range (sl):
-        telnet=Netmiko(device_type="cisco_ios_telnet",ip=ip_sv,port=porttel,secret="321",blocking_timeout=16)
+        telnet=ConnectHandler(device_type="cisco_ios_telnet",ip=ip_sv,port=porttel,secret="321",blocking_timeout=16)
         telnet.enable()
         print("Configuring Device "+str(x+1)+"...")
         telnet.send_config_set(config_int_e0)
         telnet.send_config_set(config_ssh)
         porttel=porttel+1
         telnet.disconnect()
-def parse(data_out):
-        output_parse=parse_output(platform="cisco_ios", command="show ip int br", data=data_out)
-        device_IP=output_parse[0])
-
 def showIp(sl,ip_sv,porttel=32769):
     list_IP=[]
-    n=1
-    f=open("log.txt", "w")
     for x in range (sl):
-        telnet=Netmiko()
-        print("Dang lay IP device "+str(n))
+        telnet=ConnectHandler(device_type="cisco_ios_telnet",ip=ip_sv,port=porttel,secret="321",blocking_timeout=16)
+        output=telnet.send_command("show ip int br")
+        output_parse=parse_output(platform="cisco_ios", command="show ip int br", data=output)
+        device_IP=output_parse[0]
+        list_IP.append(device_IP["ipaddr"])
         porttel=porttel+1
-        n=n+1
-        output=telnet.send_command("show ip int br",delay_factor=0)
-        parse(output)
-        #
-    #   device_IP=output_parse[0]
-    #    list_IP.append(device_IP["ipaddr"])
-
         telnet.disconnect()
-'''
-    print("Dang xuat ra file...")
-
-    #f.write("="*50+str(datetime.now())+"="*50+"\n")
+    f=open("log.txt", "w")
+    f.write("="*50+str(datetime.now())+"="*50+"\n")
     for x in list_IP:
         f.write(str(x)+"\n")
     f.close()
-'''
 if __name__ == '__main__':
     ip_sv=input("Nhap IP server EVE-NG: ")
     sl=int(input("So luong thiet bi co trong so do lab: "))
