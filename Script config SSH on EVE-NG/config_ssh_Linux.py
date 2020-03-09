f85b9aa748326ba7acee4080e327dfdf55d946ff
BlackDrag0n <pmk.drag0n.1998@gmail.com>
2020-03-09 22:16:32 +0700

    Final Linux Version 2.0

--- Script config SSH on EVE-NG/config_ssh_Linux.py
+++ Script config SSH on EVE-NG/config_ssh_Linux.py
@@ -2,8 +2,12 @@ from multiprocessing import Process
 from datetime import datetime
 from netmiko import ConnectHandler
 from ntc_templates.parse import parse_output
+import getpass
 import sys
 import time
+from os import system
+def clear():
+    _=system("clear")
 def parse(data_p):
     output_parse=parse_output(platform="cisco_ios", command="show ip int br", data=data_p)
     device_IP=output_parse[0]
@@ -13,11 +17,12 @@ def ansible_file():
     f=open("hosts","w")
     f.write("[all:vars]\n")
     f.write("ansible_user=admin\n")
-    f.write("ansible_password=123\n")
+    f.write("ansible_password="+str(master_pass)+"\n")
     f.write("ansible_connection=network_cli\n")
     f.write('ansible_become="yes"\n')
     f.write('ansible_become_method="enable"\n')
     f.write("ansible_network_os=ios\n")
+    f.write("ansible_become_password=321\n")
     f.write("[device]\n")
     f.close()
 def show_ip(a_device):
@@ -25,8 +30,9 @@ def show_ip(a_device):
     f=open("hosts","a")
     telnet = ConnectHandler(**a_device)
     output=telnet.send_command_expect("show ip int br")
-    f.write(parse(output)+"\n")
+    f.write(parse(output)+"\t#"+a_device["port"]+"\n")
     f.close()
+    return list_IP
 def config_ssh(a_device):
     config_int_e0=["int e0/0",
     "no sw",
@@ -35,12 +41,13 @@ def config_ssh(a_device):
     ]
     config_ssh=[
     "enable pass 321",
-    "username admin pass 123",
+    "username admin pass "+str(master_pass),
     "ip domain-name pmk",
     "crypto key ge rsa modul 1024",
-    "line vty 0 15",
+    "line vty 0 4",
     "login local",
-    "pass 123"
+    "pass "+str(master_pass),
+    "transport input all"
     ]
     telnet = ConnectHandler(**a_device)
     telnet.enable()
@@ -48,9 +55,13 @@ def config_ssh(a_device):
     telnet.send_config_set(config_ssh)
     telnet.disconnect()
 def list_menu():
+    print("#"*10+"\t IP Server EVE-NG: "+str(ip_sv)+"\t"+10*"#")
+    print("")
     print("1.Cau hinh SSH cho cac thiet bi")
     print("2.Thu nhap cac IP cua thiet bi xuat ra file hosts")
     print("0.Thoat")
+    print("")
+    print("#"*10+"\tUsername mac dinh: admin, Enable password mac dinh: 321\t"+10*"#")
 def menu():
         list_menu()
         choice=int(input("Nhap lua chon cua ban: "))
@@ -64,6 +75,8 @@ def menu():
                 procs.append(my_proc)
             time.sleep(10)
             print("Hoan tat cong viec")
+            time.sleep(5)
+            clear()
             menu()
         elif choice==2:
             print("Bat dau thi nhap IP va xuat ra file")
@@ -76,6 +89,8 @@ def menu():
                 procs.append(my_proc)
             time.sleep(10)
             print("Hoan tat cong viec")
+            time.sleep(5)
+            clear()
             menu()
         elif choice==0:
             sys.exit
@@ -84,6 +99,7 @@ def menu():
 if __name__ == "__main__":
     ip_sv=input("Nhap IP server EVE-NG: ")
     sl=int(input("Nhap so luong thiet bi co trong so do mang: "))
+    master_pass=getpass.getpass("Nhap password SSH ban muon dat cho cac thiet bi: ")
     sw={
             "device_type" : "cisco_ios_telnet",
             "ip" : ip_sv,
